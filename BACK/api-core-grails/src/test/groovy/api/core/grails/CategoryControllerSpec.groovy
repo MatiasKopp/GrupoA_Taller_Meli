4a110227afb30e67808b702927df5cde8829c7e6
ruhmling <adrian.ruhmling@mercadolibre.com>
2018-03-23 09:09:17 -0300

    fix gradle

---
+++ BACK/api-core-grails/src/test/groovy/api/core/grails/CategoryControllerSpec.groovy
@@ -0,0 +1,18 @@
+package api.core.grails
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class CategoryControllerSpec extends Specification implements ControllerUnitTest<CategoryController> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
