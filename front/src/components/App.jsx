8878e457c0b92da84656b45fd98a6f5db42a8207
MatiasKopp <matias.kopp@mercadolibre.com>
2018-03-25 02:49:05 -0300

    Agregado estilos al index

--- front/src/components/App.jsx
+++ front/src/components/App.jsx
@@ -1,24 +1,18 @@
 import React, {Component} from 'react';
 import ItemDisplay from './ItemDisplay/ItemDisplay';
 import Item from './Item/Item';
-import Trend from './Trends';
 import {
     BrowserRouter as Router,
-    Route,
-    Link
+    Route
 } from 'react-router-dom';
 
 class App extends Component {
-    constructor() {
-        super();
-    }
-
     render() {
         return (
             <Router>
                 <div>
                     <Route exact path="/" component={ItemDisplay} />
-                    <Route path="/items/:itemID" component={Item}/>
+                    <Route exact path="/items/:itemID" component={Item}/>
                 </div>
             </Router>
         );
--- front/src/components/Navbar/Navbar.jsx
+++ front/src/components/Navbar/Navbar.jsx
@@ -6,8 +6,12 @@ class Navbar extends Component {
         return(
             <nav className="navbar-melu">
                 <div className="nav-container">
-                    <h2>Kwik-E-Mart</h2>
-                    <div className="nav-right">
+                    <a href="/" className="navbarTitle">Kwik-E-Mart</a>
+                    <form className="searchBar">
+                        <input type="text" name="searchText" id="searchText" maxLength="40"/>
+                        <button className="glyphicon glyphicon-search btnSearch" type="button"></button>
+                    </form>
+                    <div className="nav-right hidden-xs">
                         <button className="btn btn-nav">Login</button>
                         <button className="btn btn-nav glyphicon glyphicon-shopping-cart"></button>
                     </div>
